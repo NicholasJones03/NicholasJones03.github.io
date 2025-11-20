af03fbaff897a93ab3e630ca4fdd61e1bc0826b4
NicholasJones03 <nicholas.alexander.jones@outlook.com>
2025-11-20 02:36:43 +0000

    Deploying to gh-pages from @ NicholasJones03/NicholasJones03.github.io@23655cf3b5575e1c124c6bed57b8cbc65e916b87 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,6 +416,11 @@ ninja.data = [{
           description: "Example of Value Investment Analysis, Metrics, and Reasoning",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_google_dcf/";
+            },},{id: "projects-the-butterfly-effect-of-biotech",
+          title: 'The Butterfly Effect of Biotech',
+          description: "Undergrad Capstone Thesis on Innovation and Market Inefficiency",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_butterfly_biotech/";
             },},{
         id: 'social-email',
         title: 'email',
