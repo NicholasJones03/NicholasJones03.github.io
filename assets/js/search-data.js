a21172e28f6078d13cfaecdd0c001d7774095c72
NicholasJones03 <nicholas.alexander.jones@outlook.com>
2025-11-20 04:48:10 +0000

    Deploying to gh-pages from @ NicholasJones03/NicholasJones03.github.io@e190859e8d414954e0d8f6d8ddd25ab549a9ba31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,11 +406,6 @@ ninja.data = [{
           description: "Example of correlation-based trading and statistically backed thesis on innovation hurt by inefficiencies in lack of funding in biotech",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_research_seminar_butterfly/";
-            },},{id: "projects-apple-erp-analysis",
-          title: 'Apple ERP Analysis',
-          description: "Qualitative analysis and reasoning behind Valuation",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_apple_erp/";
             },},{id: "projects-google-dcf-valuation-q1-2025",
           title: 'Google DCF Valuation Q1 2025',
           description: "Example of Value Investment Analysis, Metrics, and Reasoning",
